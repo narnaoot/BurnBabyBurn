9517a4adc19b65f7dbc844b4ce357ba6b2dc7421
n4bil <nnabil@gmail.com>
2019-01-26 19:15:36 -0800

    draft fire map complete: marker opacity based on acreage, popup shows size

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,7 +4,8 @@
 console.log("hello world")
 
 function getOpacity(size) {
-  // Helper function determines color based on magnitude of earthquake
+  // Helper function determines opacity of marker based on size (acreage) of fire
+  // Fires below 100 acres will not be shown
   return size > 100, 000 ? 1.0 :
     size > 75, 000 ? 0.9 :
       size > 50, 000 ? 0.8 :
@@ -12,11 +13,9 @@ function getOpacity(size) {
       size > 1000 ? 0.3 :
         size > 100 ? 0.1 :
           0.0
-
 }
 
 function createMap(fireMarkers) {
-  console.log("got here")
   // Making the basemap
   var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
@@ -42,20 +41,13 @@ function createMap(fireMarkers) {
     layers: [lightmap, fireMarkers]
   });
 
-  // // map.addLayer(fireMarkers);
-  // fireMarkers.addTo(map);
-
   // // Create layer control with both baseMaps & overlayMaps.  Add to map
   L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-
-
 }
 
-
-
 function createMarkers(fires) {
-  // Verify we have data
+  // Verify we have data, give sample record
+  console.log("Sample fire data:")
   console.log(fires[0]);
 
   // Initialize an array to hold markers
@@ -70,35 +62,24 @@ function createMarkers(fires) {
     var size = fire.FIRE_SIZE;
     var owner = fire.OWNER_DESCR;
 
-
-
-    // Only create markers for fires based on size
+    // Only create markers for fires > 100 acres
 
     if (size > 100) {
-      console.log(size);
-
 
       // Create one marker at a time
-      var singleMarker = L.marker([latitude, longitude]).bindPopup("yay");
+      var singleMarker = L.marker([latitude, longitude]).bindPopup("<h4>" + size + " acres </h4>");
 
+      // Set opacity based on size (see function above)
       singleMarker.setOpacity(getOpacity(size));
 
       // Add our new marker to the array
       fireMarkers.push(singleMarker);
 
     }
-
-
-
-
   }
   // Create layer group, pass to createMap
-  console.log("reached just before the create map function call")
   createMap(L.layerGroup(fireMarkers));
-
-
 }
-
 
 
 d3.csv("/static/data/2008_subset.csv").then(data => createMarkers(data));
