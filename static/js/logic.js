2ce66c00a65108868c8d670d2305bb8616de1d87
n4bil <nnabil@gmail.com>
2019-01-26 18:58:27 -0800

    changed marker opacity based on fire size

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,38 +3,50 @@
 
 console.log("hello world")
 
+function getOpacity(size) {
+  // Helper function determines color based on magnitude of earthquake
+  return size > 100, 000 ? 1.0 :
+    size > 75, 000 ? 0.9 :
+      size > 50, 000 ? 0.8 :
+      size > 25, 000 ? 0.7 :
+      size > 1000 ? 0.3 :
+        size > 100 ? 0.1 :
+          0.0
+
+}
+
 function createMap(fireMarkers) {
   console.log("got here")
-// Making the basemap
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.light",
-  accessToken: API_KEY
-});
+  // Making the basemap
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+  });
 
-// Create baseMaps obejct
+  // Create baseMaps obejct
 
-var baseMaps = {
-  "Light Map": lightmap
-};
+  var baseMaps = {
+    "Light Map": lightmap
+  };
 
-// Create overlap map for fires
-var overlayMaps = {
-  "Fires": fireMarkers
-};
+  // Create overlap map for fires
+  var overlayMaps = {
+    "Fires": fireMarkers
+  };
 
-var map = L.map("map-id",{
-  center: [34.4, -118.4],  
-  zoom: 4,
-  layers: [lightmap, fireMarkers]
-});
+  var map = L.map("map-id", {
+    center: [36.7378, -119.7871],
+    zoom: 6,
+    layers: [lightmap, fireMarkers]
+  });
 
-// // map.addLayer(fireMarkers);
-// fireMarkers.addTo(map);
+  // // map.addLayer(fireMarkers);
+  // fireMarkers.addTo(map);
 
-// // Create layer control with both baseMaps & overlayMaps.  Add to map
-L.control.layers(baseMaps, overlayMaps).addTo(map);
+  // // Create layer control with both baseMaps & overlayMaps.  Add to map
+  L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 
 
@@ -50,24 +62,40 @@ function createMarkers(fires) {
   var fireMarkers = [];
 
   // Loop through the array
-  for (var index = 0; index < 50; index++) {
+  for (var index = 0; index < fires.length; index++) {
     // Assign variables for readability
     var fire = fires[index];
     var latitude = fire.LATITUDE;
     var longitude = fire.LONGITUDE;
-    console.log(latitude, longitude)
+    var size = fire.FIRE_SIZE;
+    var owner = fire.OWNER_DESCR;
 
-    // Create one marker at a time
-    var singleMarker = L.marker([latitude, longitude]).bindPopup("yay");
 
-    // Add our new marker to the array
-    fireMarkers.push(singleMarker);
- 
+
+    // Only create markers for fires based on size
+
+    if (size > 100) {
+      console.log(size);
+
+
+      // Create one marker at a time
+      var singleMarker = L.marker([latitude, longitude]).bindPopup("yay");
+
+      singleMarker.setOpacity(getOpacity(size));
+
+      // Add our new marker to the array
+      fireMarkers.push(singleMarker);
+
+    }
+
+
+
+
   }
   // Create layer group, pass to createMap
   console.log("reached just before the create map function call")
   createMap(L.layerGroup(fireMarkers));
- 
+
 
 }
 
